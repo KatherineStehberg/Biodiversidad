4c8dc26f185de555e72ee0effe83d6803759127c
Katherine Stehberg <kstehberg@gmail.com>
2024-08-25 02:00:32 -0400

    included bundle bootstrap

--- Frontend/Src/main.jsx
+++ Frontend/Src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Importa el archivo de estilos globales
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // Montando la aplicación en el elemento con id 'root' en el index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
