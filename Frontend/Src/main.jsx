5d51da016fcd52dc47baffb67f2411a87dded5e2
Katherine Stehberg <kstehberg@gmail.com>
2024-08-25 01:58:42 -0400

    updated main

--- Frontend/Src/main.jsx
+++ Frontend/Src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Importa el archivo de estilos globales
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Montando la aplicación en el elemento con id 'root' en el index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
